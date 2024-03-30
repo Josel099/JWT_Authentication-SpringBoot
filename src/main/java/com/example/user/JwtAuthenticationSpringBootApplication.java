772f8f170669786687281b9c3978bd2db3dfb5d5
Josel Xavier <joselxavior@bmce.ac.in>
2024-03-31 00:46:51 +0530

    intial commit

---
+++ src/main/java/com/example/user/JwtAuthenticationSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticationSpringBootApplication.class, args);
+	}
+
+}
