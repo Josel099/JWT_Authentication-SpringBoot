f15ae0a0128ff82506bf3f9f3f5f265873d8153a
Josel Xavier <joselxavior@bmce.ac.in>
2024-04-01 00:19:49 +0530

    Security configuration class

---
+++ src/main/java/com/example/user/service/UserDetailsServiceImp.java
@@ -0,0 +1,42 @@
+package com.example.user.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.user.repository.UserRepository;
+
+
+
+@Service
+public class UserDetailsServiceImp implements UserDetailsService{
+	
+	
+	@Autowired //custom annotation
+	private final UserRepository userRepository ;
+	
+	// constructor 
+	public UserDetailsServiceImp (UserRepository userRepository) {
+		this.userRepository=userRepository;
+	}
+	
+
+	
+	
+	
+    // Implementation of the loadUserByUsername method from UserDetailsService interface
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		/**=============================================================================
+		 *  Call the findByUsername method of UserRepository to find a user by user name
+         * If user is found, return the UserDetails object representing the user
+         * If user is not found, throw a UsernameNotFoundException
+         ==============================================================================*/
+
+		return userRepository.findByUsername(username).orElseThrow(()->new UsernameNotFoundException("user not found"));
+	}
+
+}
